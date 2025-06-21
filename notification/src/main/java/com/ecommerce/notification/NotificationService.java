59441ba15b3fdc36c60655dfd57856c2a000fd17
João Vitor <joaovitoralves2009@hotmail.com>
2025-06-21 14:43:19 -0300

    feat: add logger no notification service

--- notification/src/main/java/com/ecommerce/notification/NotificationService.java
+++ notification/src/main/java/com/ecommerce/notification/NotificationService.java
@@ -4,6 +4,8 @@ import com.ecommerce.notification.dto.PedidoRecebido;
 import com.ecommerce.notification.dto.PedidoRecebidoNotificacao;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.mail.MailSender;
@@ -18,6 +20,7 @@ import static com.ecommerce.notification.SendEmailService.gerarHtmlConfirmacaoPe
 @Service
 public class NotificationService {
 
+    private static final Logger log = LoggerFactory.getLogger(NotificationService.class);
     private final ObjectMapper objectMapper;
 
     private final SendEmailService sendEmailService;
@@ -32,15 +35,13 @@ public class NotificationService {
             topics = "${kafka.topic.consumer}",
             groupId = "${group.id}")
     public void consumeMessage(String message) throws JsonProcessingException {
-        System.out.println("Mensagem recebida=========\n" + message);
         PedidoRecebidoNotificacao pedido = objectMapper.readValue(message, PedidoRecebidoNotificacao.class);
         Set<String> produtos = pedido.getProdutos().keySet();
-        List<String> produtosList = produtos.stream().toList();
 
         String htmlContent = gerarHtmlConfirmacaoPedido(pedido);
 
 // 2. Envie o e-mail com o conteúdo HTML
-
         sendEmailService.sendEmail("reis23@discente.ufg.br", "Pedido no Ecommerce " + pedido.getIdPedido(), htmlContent, "", true);
+        log.info("Pedido do Ecommerce " + pedido.getIdPedido() + " notificado como " + (pedido.isValido() ? "valido" : "inválido"));
     }
 }
